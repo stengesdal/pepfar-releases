750044602bea8fc172fed61ec1ff4230e5757271
Mark Polak <mark@thedutchies.com>
2014-11-27 01:17:55 +0100

    update

--- app.js
+++ app.js
@@ -2,13 +2,48 @@ angular.module('PEPFAR.releases', []);
 
 angular.module('PEPFAR.releases').controller('appController', appController);
 function appController(releaseService) {
-
+    releaseService.getReleases().then(function (releases) {
+        console.log(releases);
+    });
 }
 
 angular.module('PEPFAR.releases').service('releaseService', releaseService);
 function releaseService ($http) {
-    return $http.get('releases.json').then(function (releases) {
-        console.log(releases);
-        return releases;
-    });
+    return {
+        getReleases: getReleases
+    };
+
+    function getReleases () {
+        return loadReleases()
+            .then(function (releases) {
+                return loadManifests(releases);
+            });
+    }
+
+    function loadReleases () {
+        return $http.get('releases.json').then(function (releases) {
+            if (Array.isArray(releases.data)) {
+                return releases.data;
+            }
+            return [];
+        });
+    }
+
+    function loadManifests (releases) {
+        var promises = [];
+
+        releases.forEach(function (release) {
+            promises.push($http(release.manifest).then(function (manifest) {
+                return manifest.data;
+            }));
+        });
+
+        return $q.all(promises).then(function (manifests) {
+            manifests.forEach(function (manifest, index) {
+               releases[index].manifestData = manifest; 
+            });
+
+            return releases;
+        });
+    }
 }
